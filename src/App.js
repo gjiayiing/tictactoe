cc54a4db31a86412ec40183e0fc8be5533cfb5b7
gjiayiing <jygoh.2019@sis.smu.edu.sg>
2023-08-23 16:08:38 +0800

    update x

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+function Square() {
+  const [value, setValue] = useState(null);
+  //inner functions of each square
+  function handleClick(){
+    setValue('X');
+    console.log("clicked!"+ {value} );
+  }
 
-function App() {
+  return <button className="square" onClick={handleClick}>{value}</button>
+}
+export default function Board() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <div className="board-row">
+    <Square />
+    <Square />
+    <Square />
+  </div>
+  <div className="board-row">
+    <Square />
+    <Square />
+    <Square />
+  </div>
+  <div className="board-row">
+    <Square />
+    <Square />
+    <Square />
+  </div>
+  </>
   );
 }
-
-export default App;
