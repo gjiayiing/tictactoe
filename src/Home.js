5ecd4b3940e12b10ce823bb37a1e2b353f3e731f
gjiayiing <jygoh.2019@sis.smu.edu.sg>
2023-08-24 15:37:54 +0800

    hook update

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 const Home = () => {
+    const [blogs, setBlogs] = useState([]);
+
+
     const [name, setName] = useState('mario'); // variable and function to do whatever after
     const [age, setAge] = useState(25); // variable and function to do whatever after
     // let name = 'luigi'; // not reactive, react doesnt follow it and re-render if it changes
-        
+    
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id != id)
+        setBlogs(newBlogs);
+    }
+
+    useEffect(() => {//runs on every render
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+            return res.json();
+        })
+        .then(data=> {
+            console.log(data);
+            setBlogs(data)
+        })
+    }, []); // will run function only if name is changed
+
     const handleClick = () => {
         // name = 'mario' 
         setName('luigi');
@@ -15,12 +35,19 @@ const Home = () => {
     }
     return (  
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name } is { age } years old</p>
-            <button onClick={handleClick}>Click me</button>
-            <button onClick={(e) => {clickName('bjorn', e)}}>Click me 2 times</button>
-            {/* <button onClick={clickName('bjorn')}>Click me for name</button> */}
+            {/* prop */}
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>}
+            <button onClick={()=> setName('luigi')}>change name</button>
+            <p>{ name }</p>
+            {/* <BlogList blogs={blogs.filter((blog)=> blog.author =="mario")} title="Mario's Blogs!"/>  */}
         </div>
+        // <div className="home">
+        //     <h2>Homepage</h2>
+        //     <p>{ name } is { age } years old</p>
+        //     <button onClick={handleClick}>Click me</button>
+        //     <button onClick={(e) => {clickName('bjorn', e)}}>Click me 2 times</button>
+        //     {/* <button onClick={clickName('bjorn')}>Click me for name</button> */}
+        // </div>
     );
 }
  
